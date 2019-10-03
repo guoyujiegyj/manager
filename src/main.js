e147127fed972e8214fd01b03670c26a5aa0998c
gyj <1401357358@qq.com>
2019-10-03 14:24:03 +0800

    登录组件创建完成。

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+// ui组件导入
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import router from './router'
 
+// 使用UI组件
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
