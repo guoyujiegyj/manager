b0113f1e6717e5238f73c9133986f9eaa49b5d3a
gyj <1401357358@qq.com>
2019-10-07 20:20:19 +0800

    token认真

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -20,7 +20,73 @@
             </el-row>
         </el-header>
         <el-container>
-            <el-aside class="aside" width="200px">Aside</el-aside>
+            <el-aside class="aside" width="200px">
+                <el-menu unique-opened>
+                    <el-submenu index="1">
+                        <template slot="title">
+                            <i class="el-icon-location"></i>
+                            <span>用户管理</span>
+                        </template>
+                        <el-menu-item index="1-1">
+                            <i class="el-icon-success"></i>
+                            <span>用户列表</span>
+                        </el-menu-item>
+
+                    </el-submenu>
+                    <el-submenu index="2">
+                        <template slot="title">
+                            <i class="el-icon-location"></i>
+                            <span>权限管理</span>
+                        </template>
+                        <el-menu-item index="1-1">
+                            <i class="el-icon-success"></i>
+                            <span>角色列表</span>
+                        </el-menu-item>
+                        <el-menu-item index="1-2">
+                            <i class="el-icon-success"></i>
+                            <span>权限列表</span>
+                        </el-menu-item>
+                    </el-submenu>
+                    <el-submenu index="3">
+                        <template slot="title">
+                            <i class="el-icon-location"></i>
+                            <span>商品管理</span>
+                        </template>
+                        <el-menu-item index="1-1">
+                            <i class="el-icon-success"></i>
+                            <span>商品列表</span>
+                        </el-menu-item>
+                        <el-menu-item index="1-2">
+                            <i class="el-icon-success"></i>
+                            <span>分类参数</span>
+                        </el-menu-item>
+                         <el-menu-item index="1-3">
+                            <i class="el-icon-success"></i>
+                            <span>商品分类</span>
+                        </el-menu-item>
+                    </el-submenu>
+                    <el-submenu index="4">
+                        <template slot="title">
+                            <i class="el-icon-location"></i>
+                            <span>订单管理</span>
+                        </template>
+                        <el-menu-item index="1-1">
+                            <i class="el-icon-success"></i>
+                            <span>订单列表</span>
+                        </el-menu-item>
+                    </el-submenu>
+                    <el-submenu index="5">
+                        <template slot="title">
+                            <i class="el-icon-location"></i>
+                            <span>数据统计</span>
+                        </template>
+                        <el-menu-item index="1-1">
+                            <i class="el-icon-success"></i>
+                            <span>数据1</span>
+                        </el-menu-item>
+                    </el-submenu>
+                </el-menu>
+            </el-aside>
             <el-main class="main">Main</el-main>
         </el-container>
     </el-container>
@@ -28,7 +94,19 @@
 
 <script>
 export default {
+  data () {
+    return {
 
+    }
+  },
+  beforeCreate () {
+      // 判断 是否有token。如果没有直接返回登录页。
+      // token是防止通过粘贴地址来进行访问此页。因为有可能没有登陆。
+    const token = localStorage.getItem('token')
+    if (!token) {
+      this.$router.push('/login')
+    }
+  }
 }
 </script>
 
@@ -38,7 +116,7 @@ export default {
     }
     .header{
         background:#b3c0d1;
-        
+
         height:60px;
     }
     .aside{
@@ -49,7 +127,7 @@ export default {
     }
     .middle{
         text-align: center;
-       
+
         line-height: 60px;
     }
     .middle>h3{
@@ -62,6 +140,6 @@ export default {
     }
     .loginout>a{
         text-decoration: none;
-        color:rgb(70, 68, 68);
+        color:#e43936;
     }
 </style>
