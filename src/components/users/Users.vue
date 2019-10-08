d98bf26f5b6e6cf2a43f968b25c785ede76b6d7e
gyj <1401357358@qq.com>
2019-10-08 15:33:26 +0800

    用户列表获取渲染，日期过滤，slot-scope。

--- src/components/users/Users.vue
+++ src/components/users/Users.vue
@@ -1,14 +1,104 @@
 <template>
-  <div>
-    users
-  </div>
+  <el-card class="box-card">
+      <!--面包屑导航-->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item>首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 搜索框-->
+    <el-input placeholder="请输入内容" v-model="query" class="input-with-select">
+      <el-button slot="append" icon="el-icon-search"></el-button>
+    </el-input>
+    <el-button type="primary" plain>添加用户</el-button>
+    <!--表格-->
+    <el-table
+      :data="userlist"
+      style="width: 100%">
+      <el-table-column
+      type="index"
+     
+        label="#"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="username"
+        label="姓名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="email"
+        label="邮箱">
+      </el-table-column>
+      <el-table-column
+        prop="mobile"
+        label="电话">
+      </el-table-column>
+      <!-- 作用域插槽 slot-scope,   -->
+      <el-table-column
+        label="创建时间">
+        <!--通过slot-scope来获取源数据。-->
+        <template slot-scope="userlist">
+         {{userlist.row.create_time|fmtdate}}
+        </template>
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="用户状态">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="操作">
+      </el-table-column>
+    </el-table>
+  </el-card>
 </template>
 
 <script>
 export default {
-
+  data(){
+    return{
+     userlist: [],
+     // 分页数据
+     pagenum: 1,
+     pagesize: 3,
+     query: '',
+     total: -1,
+    }
+  },
+  created(){
+   this.getUrlList()
+  },
+  methods:{
+    async getUrlList(){
+      // 获取token值。
+      const AUTH_TOKEN=localStorage.getItem('token')
+      // 设置请求头。
+      this.$http.defaults.headers.common['Authorization'] = AUTH_TOKEN
+      // 发送请求。
+      const res = await this.$http.get(
+        `users?query=${this.query}&pagenum=${this.pagenum}&pagesize=${this.pagesize}`)
+      // console.log(res)
+       const {meta:{status,msg},data:{total,users}} = res.data
+       if(status === 200){
+         this.userlist = users
+        //  console.log(this.userlist)
+         this.total=total
+         this.$message.success(msg)
+       }else{
+         this.$message.warning(msg)
+       }
+    }
+  }
 }
 </script>
 
 <style scoped>
+  .box-card{
+    height:100%; 
+  }
+  .input-with-select{
+    width:400px;
+    margin-top:20px;
+  }
 </style>
