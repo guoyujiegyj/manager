793e8a15e7533442d60e7eb4587edde2dca8c798
gyj <1401357358@qq.com>
2019-10-10 12:11:00 +0800

    删除角色的权限成功

--- src/components/right/Role.vue
+++ src/components/right/Role.vue
@@ -10,24 +10,27 @@
                 <el-row v-for='(item1, id) in scope.row.children' :key="id">
                     <!--第一列-->
                     <el-col :span='4'>
-                        <el-tag type="success" >{{item1.authName}}</el-tag>
+                        <el-tag @close='deleteRight(scope.row.id,item1.id)' closable type="success" >{{item1.authName}}</el-tag>
+                        <i class='el-icon-arrow-right'></i>
                     </el-col>
                     <!--第二列：后面的两大列。-->
                     <el-col :span='20'>
                         <!--此处的行是视觉上第二列的行-->
                         <el-row v-for='(item2,i) in item1.children' :key='i'>
                             <el-col :span='4'> 
-                                <el-tag type="info">{{item2.authName}}</el-tag>
+                                <el-tag @close='deleteRight(scope.row.id,item2.id)' closable type="info">{{item2.authName}}</el-tag>
+                                <i class='el-icon-arrow-right'></i>
                             </el-col>
                             <!--最后一列-->
                             <el-col :span="20">
-                                <el-tag type="warning " v-for='(item3,i) in item2.children' :key='i'>
+                                <el-tag @close='deleteRight(scope.row.id,item3.id)' closable type="warning " v-for='(item3,i) in item2.children' :key='i'>
                                     {{item3.authName}}
                                 </el-tag>
                             </el-col>
                         </el-row>
                     </el-col>
                 </el-row>
+                <span class='span1' v-if='scope.row.children.length===0'>未分配权限</span>
           </template>
       </el-table-column>   
       <el-table-column type="index" label="#" width="100"></el-table-column>
@@ -56,17 +59,32 @@ export default {
         }
     },
     created() {
-        this.getRoleList()
+        this.getRightList()
     },
     methods: {
-        async getRoleList() {
+        // 获取角色列表
+        async getRightList() {
             const res = await this.$http.get('roles')
             this.roleList=res.data.data
+            // console.log(res)
+        },
+        // 删除权限请求。
+        async deleteRight(roleId,rightId) {
+            // roles/:roleId/rights/:rightId
+            const res = await this.$http.delete(`roles/${roleId}/rights/${rightId}`)
             console.log(res)
+            if(res.data.meta.status===200) {
+                this.$message.success(res.data.meta.msg)
+                // 重新获取数据
+                this.getRightList()
+            }
         }
     }
 }
 </script>
 <style lang="" scoped>
-    
+    .span1{
+        margin-left:400px;
+    }
+ 
 </style>
